8760af1441cae4267a3f77e83162d0e9ceeb8a86
Muhammad Osama <61136831+Osama-285@users.noreply.github.com>
2024-03-05 23:20:00 +0500

    signup mapping

--- Auth/src/main/java/org/authentication/Auth/Controller/AuthController.java
+++ Auth/src/main/java/org/authentication/Auth/Controller/AuthController.java
@@ -1,10 +1,23 @@
 package org.authentication.Auth.Controller;
 
+import org.authentication.Auth.Payload.AccountDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api")
 public class AuthController {
-    
+    @PostMapping(value = "/signup", produces = "application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Add a new User")
+    public ResponseEntity<String> addUser(@Valid @RequestBody AccountDTO accountDTO) {
+
+    }
 }
